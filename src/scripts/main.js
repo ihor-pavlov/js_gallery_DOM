b73fc005aa281645f837390d9bb4b5f08b6b9d2e
Ihor Pavlov <ihorpavlov@gmail.com>
2025-01-16 12:22:44 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const thumbs = document.getElementById('thumbs');
+const largeImg = document.getElementById('largeImg');
+
+thumbs.addEventListener('click', () => {
+  event.preventDefault();
+
+  const link = event.target.closest('a');
+
+  if (!link) {
+    return;
+  }
+
+  largeImg.src = link.href;
+  largeImg.alt = link.title;
+});
